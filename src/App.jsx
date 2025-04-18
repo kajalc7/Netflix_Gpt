c0e52c2ddfe8a01b98985762bb110ac874f5dfac
kajalc7 <79201873+kajalc7@users.noreply.github.com>
2025-04-18 13:15:38 +0530

    configured tailwind and updated readme file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import React from "react"
 function App() {
 
-  return (
-    <>
-      <div>
-        <p>Hello everyone lets build netflix along with chatgpt</p>
-      </div>
-    </>
-  )
+  return <div className="text-3xl font-bold text-green-400">
+          Hello everyone lets build netflix along with chatgpt1
+         </div>;    
 }
 
 export default App
