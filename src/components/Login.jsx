35138ecc1dedb0882ed350a31339499f03d47339
kajalc7 <79201873+kajalc7@users.noreply.github.com>
2025-04-18 23:58:45 +0530

    useRef hook for form validation of input field

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,24 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import Header from './Header'
+import { checkValidData } from '../utils/validate'
 
 const Login = () => {
   const[isSignInForm,setisSignInForm]= useState(true)
+  const[errormessage,seterrormessage]=useState(null);
+
+  const email= useRef(null);
+  const password = useRef(null);
 
   const handleButtonClick=()=>{
     //validate form data will be using utility which we have created under utils
-    //checkValidData()
+    //now we want reference of input field and password as onsignin click we need to get the field data entered and validate for that 
+    //we are using ref hook
+    console.log(email.current.value);
+    console.log(password.current.value);
+
+    const message = checkValidData(email.current.value,password.current.value);
+   // console.log(message);
+    seterrormessage(message);
 
   }
 
@@ -23,7 +35,8 @@ const Login = () => {
        "https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg"
       alt="BG_logo"/>
       </div>
-      <form className="w-3/12 absolute p-12 bg-[rgba(0,0,0,0.7)]  my-36  mx-auto right-0 left-0 text-white rounded-lg">
+      <form  onSubmit={(e)=> e.preventDefault()}//this will help us to prevent to submit the form and will prevent reloading also
+      className="w-3/12 absolute p-12 bg-[rgba(0,0,0,0.7)]  my-36  mx-auto right-0 left-0 text-white rounded-lg">
       <h1 className="font-bold text-3xl py-4">
         {isSignInForm? "Sign In" : "Sign Up"}
         </h1>
@@ -33,16 +46,19 @@ const Login = () => {
         className="w-full p-3 mb-6 rounded bg-gray-800 placeholder-gray-400 focus:outline-none"
       />}
       <input
+          ref={email}
           type="text"
           placeholder="Email or Mobile number"
           className="w-full p-3 mb-4 rounded bg-gray-800 placeholder-gray-400 focus:outline-none"
         />
         <input
+          ref={password}
           type="password"
           placeholder="Password"
           className="w-full p-3 mb-6 rounded bg-gray-800 placeholder-gray-400 focus:outline-none"
         />
         
+        <p className="text-red-400 font-semibold text-lg  py-3"> {errormessage}</p>
         <button className="w-full bg-red-600 hover:bg-red-700 p-3 rounded font-semibold cursor-pointer" onClick = {handleButtonClick} >
           {isSignInForm? "Sign In" : "Sign Up"}
         </button>
