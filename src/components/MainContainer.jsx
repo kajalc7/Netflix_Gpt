f7523f9720bf26e604d21cec8e86d68ccfa062d5
kajalc7 <79201873+kajalc7@users.noreply.github.com>
2025-04-26 18:03:14 +0530

    video title css

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -4,14 +4,17 @@ import VideoTitle from './VideoTitle'
 import VideoBackground from './Videobackground'
 
 const MainContainer = () => {
-    const movies = useSelector((store) => store.movies?.nowPlayingMovies);
-    if (!movies) return null;
+    const movies = useSelector((store) => store.movies?.nowPLayingMovies);
+    if(!movies) return;
+    
     const mainMovie = movies[0];
     console.log(mainMovie);
 
+    const{original_title, overview} = mainMovie;
+
   return (
     <div>
-      <VideoTitle/>
+      <VideoTitle title ={original_title} overview ={overview}/>
       <VideoBackground />
     </div>
   );
--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
-const VideoTitle = () => {
+const VideoTitle = ({title, overview}) => {
   return (
-    <div>
-      videoTitle
+    <div className="pt-36 px-12">
+      <h1 className="text-6xl font-bold">{title}</h1>
+      <p className="py-6 text-lg w-1/4">{overview}</p>
+      <div classname=" ">
+        <button className="bg-gray-500 text-white p-4 px-12 text-xl opacity-50 rounded-lg"> ▶ Play</button>
+        <button className="bg-gray-500 mx-2 text-white p-4 px-12 text-xl opacity-50 rounded-lg">
+          More Info
+        </button>
+      </div>
     </div>
   )
 }
