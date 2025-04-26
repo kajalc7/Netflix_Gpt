2c595d6a6781e8df5d84aa02908152900e599044
kajalc7 <79201873+kajalc7@users.noreply.github.com>
2025-04-26 22:29:15 +0530

    trailer video using redux store and removed usestate

--- src/components/Videobackground.jsx
+++ src/components/Videobackground.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { API_Options } from '../utils/constants'
+import { useDispatch, useSelector } from 'react-redux';
+import { addTrailerVideo } from '../utils/movieSlice';
 
 const VideoBackground = ({movieId}) => {
-
-  const[trailerId, setTrailerId] = useState(null);
+  const trailerVideo = useSelector(store=>store.movies?.trailerVideo)
+  const dispatch=useDispatch(null);
 //fetch trailer video
 
 const getmovieVideos = async () =>{
@@ -14,7 +16,7 @@ const getmovieVideos = async () =>{
    const filterData = json.results.filter((video)=> video.type==="Trailer");
    const trailer = filterData.length ? filterData[0] :json.results[0];
    console.log(trailer);
-   setTrailerId(trailer.key)
+   dispatch(addTrailerVideo(trailer));
 }
 
 useEffect(() =>{
@@ -24,7 +26,7 @@ useEffect(() =>{
   return (
     <div>
       <iframe width="560" height="315" 
-      src={"https://www.youtube.com/embed/"+trailerId} 
+      src={"https://www.youtube.com/embed/"+ trailerVideo?.key} 
       title="YouTube video player" 
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
       referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
--- src/utils/movieSlice.jsx
+++ src/utils/movieSlice.jsx
@@ -4,14 +4,18 @@ const moviesSlice = createSlice({
     name: "movies",
     initialState : {
         nowPLayingMovies: null,
+        trailerVideo:null
     },
     reducers: {
         addNowPlayingMovies :(state, action) => {
             state.nowPLayingMovies = action.payload;
         },
+        addTrailerVideo : (state,action) =>{
+            state.trailerVideo=action.payload;
+        }
     },
 });
 
 
-export const {addNowPlayingMovies} =  moviesSlice.actions;
+export const {addNowPlayingMovies, addTrailerVideo} =  moviesSlice.actions;
 export default moviesSlice.reducer;
